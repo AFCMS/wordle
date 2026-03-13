45162fab28c44ffc3e76c00b1fea4a2003b29e10
AFCMS <afcm.contact@gmail.com>
2026-03-13 20:12:21 +0100

    Add `vite-plugin-sri-gen`

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,19 @@
 import { defineConfig } from "vite";
 import { preact } from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import sri from "vite-plugin-sri-gen";
 
 export default defineConfig({
-	plugins: [tailwindcss(), preact()]
+    plugins: [
+        tailwindcss(),
+        preact(),
+        sri({
+            algorithm: "sha512",
+            crossorigin: "anonymous",
+            fetchCache: true,
+            fetchTimeoutMs: 5000,
+            skipResources: [],
+            verboseLogging: false,
+        }),
+    ],
 });
